9d7ee835040b3b40ea2353b6dcdb2ced5eef5bed
Daisuke Nishide <d.nishide0125@gmail.com>
2014-12-28 18:34:27 +0800

    Add dependency package installation settings to php recipe.

--- site-cookbooks/php/recipes/default.rb
+++ site-cookbooks/php/recipes/default.rb
@@ -7,6 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 
+package "libmcrypt" do
+    action :install
+    options "--enablerepo=epel"
+end
+
 packages = %w(php php-devel php-pear php-mbstring php-xml php-mcrypt php-gd php-pecl-xdebug php-opcache php-pecl-apcu php-phpunit-PHPUnit php-mysqlnd)
 
 packages.each do |p|
