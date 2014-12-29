231fc5d25136806e41b80892d5f4daef134bfcbc
Daisuke Nishide <d.nishide0125@gmail.com>
2014-12-29 21:44:45 +0800

    Add configurations about mysql template.

--- site-cookbooks/mysql/recipes/default.rb
+++ site-cookbooks/mysql/recipes/default.rb
@@ -18,3 +18,12 @@ end
 service "mysqld" do
     action [:enable, :start]
 end
+
+template "my.cnf" do
+    owner "root"
+    group "root"
+    mode 0644
+    path "/etc/my.cnf"
+    source "my.cnf.erb"
+    notifies :restart, 'service[mysqld]'
+end
